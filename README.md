53e2de34c2459d91a0867c15129e398306b61299
Bryan Flores <bryan_flores7@outlook.com>
2022-05-30 17:57:11 -0500

    Plantilla

--- README.md
+++
@@ -1,2 +0,0 @@
-# debraye
-Portafolio profesional
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 const express = require('express');
-
 const path = require('path');
 
 // initializations
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 // Configuracion
 app.set('port', process.env.PORT || 3000);
 
-// Routes
-app.use(require('./routes'));
-// app.use('/torneos', require('./routes/torneos'));
-
 // Public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // Servidor
 app.listen(app.get('port'), ()=>{
